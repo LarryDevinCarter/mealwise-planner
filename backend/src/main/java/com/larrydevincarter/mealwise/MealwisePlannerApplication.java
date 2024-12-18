866b763509efbdf39f8d5f8b94995a8bff3f4320
LarryDevinCarter <larrydevincarter@gmail.com>
2024-12-18 10:46:12 -0600

    Generated backend

---
+++ backend/src/main/java/com/larrydevincarter/mealwise/MealwisePlannerApplication.java
@@ -0,0 +1,13 @@
+package com.larrydevincarter.mealwise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealwisePlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MealwisePlannerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/larrydevincarter/mealwise/MealwisePlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.larrydevincarter.mealwise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealwisePlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
